f06c352bfd54edbb89bafa0bc298af3dce577ce3
Jomariel Gaitera <jojogaitera@gmail.com>
2023-12-19 21:37:42 +0800

    Fix bug

--- src/ws.ts
+++ src/ws.ts
@@ -35,6 +35,16 @@ export async function ws(httpServer: HttpServer) {
       mqtt.subscribe(topic);
     });
 
+    socket.on('mqtt:unsubscribe', async (topic: string) => {
+      const [, key] = topic.split(':');
+      const devices = await deviceService.getDevicesByUserId(user.id);
+      const keys = devices.map((device) => device.deviceKey);
+
+      if (!keys.includes(key)) return;
+
+      mqtt.unsubscribe(topic);
+    });
+
     socket.on('mqtt:publish', async (topic: string, message: string) => {
       const [, key] = topic.split(':');
       const devices = await deviceService.getDevicesByUserId(user.id);
@@ -44,75 +54,67 @@ export async function ws(httpServer: HttpServer) {
 
       mqtt.publish(topic, message);
     });
+  });
 
-    // Note: Only subscribed topics through the application will be captured
-    mqtt.on('message', async (_topic, payload) => {
-      const [topic, key] = _topic.split(':');
-      const message = payload.toString();
-      const device = await deviceService.getDeviceByKey(key);
+  // Note: Only subscribed topics through the application will be captured
+  mqtt.on('message', async (_topic, payload) => {
+    const [topic, key] = _topic.split(':');
+    const message = payload.toString();
+    const device = await deviceService.getDeviceByKey(key);
 
-      if (!device) return;
+    if (!device) return;
 
-      switch (topic) {
-        case TOPICS.B_SYNC: {
-          mqtt.publish(`${TOPICS.A_SYNC}:${key}`, '');
-          mqtt.publish(
-            `${TOPICS.I_SYNC}:${key}`,
-            device.DeviceData.isRinging ? '1' : '0',
-          );
-          break;
-        }
-
-        case TOPICS.I_PRESS: {
-          await deviceService.addDeviceDataPress(device.id);
-          mqtt.publish(`${TOPICS.A_SYNC}:${key}`, '');
-          mqtt.publish(
-            `${TOPICS.I_SYNC}:${key}`,
-            device.DeviceData.isRinging ? '1' : '0',
-          );
-          break;
-        }
-
-        case TOPICS.A_RING: {
-          const states = ['OFF', 'ON'];
-          const index = states.indexOf(message);
-          const state = Boolean(index);
-          if (index < 0) return;
-          await deviceService.toggleAlarmById(device.id, state);
-          mqtt.publish(`${TOPICS.A_SYNC}:${key}`, '');
-          mqtt.publish(
-            `${TOPICS.I_SYNC}:${key}`,
-            device.DeviceData.isRinging ? '1' : '0',
-          );
-          break;
-        }
-      }
-
-      for (const [, socket] of io.sockets.sockets) {
-        const request = socket.request as unknown as Express.Request;
-        const user = await userService.getUserById(
-          request.session.userId ?? '',
+    switch (topic) {
+      case TOPICS.B_SYNC: {
+        mqtt.publish(
+          `${TOPICS.I_SYNC}:${key}`,
+          device.DeviceData.isRinging ? '1' : '0',
         );
-
-        if (!user) continue;
-
-        const devices = await deviceService.getDevicesByUserId(user.id);
-        const device = devices.find((device) => device.deviceKey === key);
-
-        if (!device) continue;
-
-        const data = {
-          timestamp: Date.now(),
-          topic: {
-            raw: _topic,
-            parsed: topic,
-          },
-          message,
-          device,
-        };
-
-        socket.emit('mqtt:message', data);
+        mqtt.publish(`${TOPICS.A_SYNC}:${key}`, '');
+        break;
       }
-    });
+
+      case TOPICS.I_PRESS: {
+        await deviceService.addDeviceDataPress(device.id);
+        mqtt.publish(`${TOPICS.B_SYNC}:${key}`, '');
+        break;
+      }
+
+      case TOPICS.A_RING: {
+        const states = ['OFF', 'ON'];
+        const index = states.indexOf(message);
+        const state = Boolean(index);
+        if (index < 0) return;
+        await deviceService.toggleAlarmById(device.id, state);
+        mqtt.publish(`${TOPICS.B_SYNC}:${key}`, '');
+        break;
+      }
+    }
+
+    if (topic !== TOPICS.A_SYNC) return;
+
+    for (const [, socket] of io.sockets.sockets) {
+      const userId = socket.handshake.auth.userId;
+      const user = await userService.getUserById(userId);
+
+      if (!user) continue;
+
+      const devices = await deviceService.getDevicesByUserId(user.id);
+      const device = devices.find((device) => device.deviceKey === key);
+
+      if (!device) continue;
+
+      const data = {
+        timestamp: Date.now(),
+        topic: {
+          raw: _topic,
+          parsed: topic,
+        },
+        message,
+        device,
+      };
+
+      socket.emit('mqtt:message', data);
+    }
   });
 }
